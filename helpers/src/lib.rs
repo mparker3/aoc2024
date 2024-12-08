43003a07d5e03d23c490ef7d607d10d4b0d14360
mparker3 <mattfparker11@gmail.com>
2024-12-08 07:42:15 -0500

    into grid helper

--- helpers/src/lib.rs
+++ helpers/src/lib.rs
@@ -1,10 +1,14 @@
-use std::{fs::File, io::{Error, Read}};
+use std::{fs::File, io::Read};
 
-pub fn get_input(path: &str) -> Result<String, Error> {
-    let mut file = File::open(path)?;
+pub fn get_input(path: &str) -> String {
+    let mut file = File::open(path).unwrap();
     let mut contents = String::new();
-    file.read_to_string(&mut contents)?;
-    Ok(contents)
+    file.read_to_string(&mut contents).unwrap();
+    contents
+}
+
+pub fn into_grid(grid_str: &str) -> Vec<Vec<char>> {
+    grid_str.split("\n").map(|x| x.chars().collect::<Vec<char>>()).collect()
 }
 
 #[cfg(test)]
@@ -13,15 +17,26 @@ mod tests {
 
     #[test]
     fn test_get_input() {
-        let result = get_input("test_data/data.txt").unwrap();
+        let result = get_input("test_data/data.txt");
         assert_eq!(result, "foo\n\
         bar\n\
         baz\n\
         qux\n");
 
-        let result = get_input("file/does/not/exist.txt");
-        assert!(result.is_err()); 
     }
 
+    #[test]
+    #[should_panic]
+    fn test_get_invalid_input() {
+        let _ = get_input("file/does/not/exist.txt");
+    }
+
+    #[test]
+    fn test_into_grid() {
+        let grid = into_grid("...\n\
+        ...\n\
+        ...");
+        assert_eq!(grid, vec![vec!['.', '.', '.'], vec!['.', '.', '.'], vec!['.', '.', '.']]);
+    }
 
 }
