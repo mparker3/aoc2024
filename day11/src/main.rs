a5bca55db310d8bedc323be2e1bc8b08d6ade14e
Matt Parker <matt.parker@hinge.co>
2024-12-11 21:16:32 -0500

    parallel isn't faster

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,49 +1,45 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, sync::{Arc, RwLock}};
 use rayon::prelude::*;
+use helpers_macros::timeit;
 
+#[timeit]
 fn main() {
     let mut input: Vec<u64> = helpers::get_input("input.txt").split_whitespace().map(|x| x.parse::<u64>().unwrap()).collect();
-    let mut cache: HashMap<(u64, u64), Vec<u64>> = HashMap::new();
+    let cache: Arc<RwLock<HashMap<(u64, u64), u64>>> = Arc::new(RwLock::new(HashMap::new()));
+
     let blinks = 75;
 
-    println!("{}", input.into_iter().flat_map(|n| rec_blink(n, blinks, &mut cache)).collect::<Vec<u64>>().len());
+    println!("{}", input.into_par_iter().map(|n| {
+        let cache_ref = Arc::clone(&cache);
+        rec_blink(n, blinks, &cache_ref)}
+    ).sum::<u64>());
 }
 
-fn rec_blink(i: u64, num_itr: u64, cache: &mut HashMap<(u64, u64), Vec<u64>>) -> Vec<u64> {
+fn rec_blink(i: u64, num_itr: u64, cache_ref: &Arc<RwLock<HashMap<(u64, u64), u64>>>) -> u64 {
+    { // read block
+        let cache = cache_ref.read().unwrap();
+        if let Some(cached) = cache.get(&(i, num_itr)) {
+            return cached.clone();
+        }
+    } // end read block
+
     if num_itr == 0 {
-        return vec![i]
+        return 1;
     }
-    if let Some(cached) = cache.get(&(i, num_itr)) {
-        println!("Cache hit for ({}, {})", i, num_itr);
-        return cached.clone();
-    }
-    println!("Cache miss for ({}, {})", i, num_itr);
-
-    let mut res: Vec<u64>;
+   
+    let res: u64;
     if i == 0 {
-        res = rec_blink(1, num_itr - 1, cache);
+        res = rec_blink(1, num_itr - 1, cache_ref);
     } else if i.to_string().len() % 2 == 0 {
         let str_form = i.to_string();
         let (j, k)= str_form.split_at(str_form.len() / 2);
-        res = rec_blink(j.parse::<u64>().unwrap(), num_itr - 1, cache);
-        res.extend(rec_blink(k.parse::<u64>().unwrap(), num_itr - 1, cache)); 
+        res = rec_blink(j.parse::<u64>().unwrap(), num_itr - 1, cache_ref) + rec_blink(k.parse::<u64>().unwrap(), num_itr - 1, cache_ref); 
     } else {
-        res = rec_blink(i* 2024, num_itr- 1, cache)
+        res = rec_blink(i* 2024, num_itr - 1, cache_ref)
     }
-
-    cache.insert((i, num_itr), res.clone());
-    res
-}
-
-
-fn blink(i: u64) -> Vec<u64>{
-    if i == 0 {
-        return vec![1];
-    } else if i.to_string().len() % 2 == 0 {
-        let str_form = i.to_string();
-        let (j, k)= str_form.split_at(str_form.len() / 2);
-        return vec![j.parse::<u64>().unwrap(), k.parse::<u64>().unwrap()];
-    } else {
-        return vec![i * 2024];
-    }
+    { // write block
+        let mut cache = cache_ref.write().unwrap();
+        cache.insert((i, num_itr), res.clone());
+        res
+    } // end write block
 }
