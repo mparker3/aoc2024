2ea3c03c189ffd6c77e8cc0e9d2ed5bee6d9e5e3
Matt Parker <matt.parker@hinge.co>
2024-12-12 19:12:48 -0500

    day 12 part 1

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{collections::{HashMap, HashSet}, mem};
 
 use helpers::into_grid;
 use helpers_macros::timeit;
@@ -8,7 +8,7 @@ pub struct Grid<T> {
     data: Vec<Vec<T>>,
 }
 
-#[derive(Debug, Clone, Hash, Eq, PartialEq)]
+#[derive(Debug, Clone, Copy, Hash, Eq, PartialEq)]
 pub struct Point {
     x: usize,
     y: usize,
@@ -58,37 +58,105 @@ impl<T: Clone + Default + Copy> From<Vec<Vec<T>>> for Grid<T> {
     }
 }
 
-#[timeit]
-fn main() {
-    let grid_str = helpers::get_input("sample1.txt");
-    let grid: Grid<_> = into_grid(&grid_str).into(); // dude what is this function signature
-    let mut sets: Vec<Vec<Point>> = Vec::new();
-    let mut group_pointers: HashMap<Point, &mut Vec<Point>> = HashMap::new();
+#[derive(Debug)]
+struct UnionFind {
+    parent: HashMap<(usize, usize), (usize, usize)>,
+    rank: HashMap<(usize, usize), usize>,
+}
 
-    for (x, y) in grid.coords() {
-        let neighbors = grid.get_neighboring_coords(x, y);
-        // check if any of the neighbors have the same value as the current cell
-        // if so, add them to the same set
-        let mut added = false; // this is bad sharing state
-        for neighbor in neighbors {
-            if grid.get(neighbor.x, neighbor.y) == grid.get(x, y) { // this is 
-                let other_set = group_pointers.get_mut(&neighbor).unwrap();
-                // mutate the underlying set and add the current cell to it
-                other_set.push(Point{x, y});
-                added = true;
-                break;
-            }   
+impl UnionFind {
+    fn new() -> Self {
+        UnionFind {
+            parent: HashMap::new(),
+            rank: HashMap::new(),
         }
-        if !added {
-            TODO(mparker): fix your mutable borrow issues
-            let mut new_vec = vec![Point{x, y}];
-            sets.push(new_vec);
-            let last_vec = sets.last_mut().unwrap();
-            group_pointers.insert(Point{x, y}, &mut last_vec);
-
-        }
-
-        // if not, create a new set
     }
 
+    fn make_set(&mut self, x: (usize, usize)) {
+        if !self.parent.contains_key(&x) {
+            self.parent.insert(x, x);
+            self.rank.insert(x, 0);
+        }
+    }
+
+    fn find(&mut self, x: &(usize, usize)) -> (usize, usize) {
+        let px = self.parent[x];
+        if px != *x {
+            let new_parent = self.find(&px);
+            self.parent.insert(*x, new_parent);
+        }
+        self.parent[x]
+    }
+
+    fn union(&mut self, x: (usize, usize), y: (usize, usize)) {
+        let x_root = self.find(&x);
+        let y_root = self.find(&y);
+
+        if x_root != y_root {
+            let x_rank = self.rank[&x_root];
+            let y_rank = self.rank[&y_root];
+
+            if x_rank > y_rank {
+                self.parent.insert(y_root, x_root);
+            } else if x_rank < y_rank {
+                self.parent.insert(x_root, y_root);
+            } else {
+                self.parent.insert(y_root, x_root);
+                self.rank.insert(x_root, x_rank + 1);
+            }
+        }
+    }
+}
+
+pub fn find_regions(grid: &Grid<char>) -> Vec<HashSet<(usize, usize)>> {
+    let mut uf = UnionFind::new();
+    
+    for (row, col) in grid.coords() {
+        uf.make_set((row, col));
+    }
+
+    for (row, col) in grid.coords() {
+        let current_value = grid.get(row, col);
+        for neighbor in grid.get_neighboring_coords(row, col) {
+            if grid.get(neighbor.x, neighbor.y) == current_value {
+                uf.union((row, col), (neighbor.x, neighbor.y));
+            }
+        }
+    }
+
+    let mut regions: HashMap<(usize, usize), HashSet<(usize, usize)>> = HashMap::new();
+    for (row, col) in grid.coords() {
+        let root = uf.find(&(row, col));
+        regions.entry(root)
+            .or_insert_with(HashSet::new)
+            .insert((row, col));
+    }
+
+    regions.into_values().collect()
+}
+
+#[timeit]
+fn main() {
+    let grid_str = helpers::get_input("input.txt");
+    let grid: Grid<_> = into_grid(&grid_str).into(); // dude what is this function signature
+    let regions = find_regions(&grid);
+    let mut total_cost = 0;
+    for region in regions {
+        let mut region_perim= 0;
+        for member in &region {
+            for neighbor in grid.get_neighboring_coords(member.0, member.1) {
+                if !region.contains(&(neighbor.x, neighbor.y)) {
+                    region_perim+= 1;   
+                } 
+            }
+            if member.0 == 0 || member.0 == grid.data.len() - 1 {
+                region_perim += 1;
+            }
+            if member.1 == 0 || member.1 == grid.data[0].len() - 1 {
+                region_perim += 1;
+            }
+        }
+        total_cost += region_perim * region.len();
+    }
+    println!("{}", total_cost);
 }
