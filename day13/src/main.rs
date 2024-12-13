1b38f77e406326bbe7792a6f1ea03cdca99d14c6
mparker3 <mattfparker11@gmail.com>
2024-12-13 11:16:10 -0500

    day 13 part 2

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -1,35 +1,35 @@
 use helpers::get_input;
 use helpers_macros::timeit;
+use nalgebra::{Matrix2, Vector2};
 use regex::Regex;
 
 #[derive(Debug)]
 struct Point {
-    x: u64,
-    y: u64
+    x: f64,
+    y: f64,
 }
 
 #[derive(Debug)]
 struct Game {
-    target: Point,
-    button_a: Point,
-    button_b: Point,
+    p: f64,
+    q: f64,
+    a_x: f64,
+    a_y: f64,
+    b_x: f64,
+    b_y: f64,
 }
 
 impl Game {
-    fn play(&self) -> Option<u64> {
-        let mut costs: Vec<_> = Vec::new();
-        for i in 0..100 {
-            for j in 0..100 {
-                if (i * self.button_a.x) + (j * self.button_b.x) == self.target.x && (i * self.button_a.y) + (j * self.button_b.y) == self.target.y {
-                    costs.push(3*i + 1*j);
-                }
-            }
-        }
-        if costs.is_empty() {
+    fn play(&self) -> Option<f64> {
+        let mtrx = Matrix2::new(self.a_x, self.b_x, self.a_y, self.b_y);
+        let vec = Vector2::new(self.p, self.q);
+        let inv = mtrx.try_inverse()?;
+        let sol = inv * vec;
+        if (sol[0] - sol[0].round()).abs() > 1e-2 || (sol[1] - sol[1].round()).abs() > 1e-2 {
             return None;
         }
-        costs.sort();
-        return Some(costs[0]);
+        println!("{:?}", sol);
+        return Some(3.0 * sol[0] + 1.0 * sol[1]);
     }
 }
 
@@ -40,22 +40,16 @@ fn main() {
     let mut games_raw = input.split("\n\n");
 
     let re = Regex::new(r"Button A: X\+(?P<a_x>\d+), Y\+(?P<a_y>\d+)\nButton B: X\+(?P<b_x>\d+), Y\+(?P<b_y>\d+)\nPrize: X=(?P<p_x>\d+), Y=(?P<p_y>\d+)").unwrap();
-    let mut total_cost = 0;
+    let mut total_cost: f64 = 0.0;
     for game_raw in games_raw {
         let captures    = re.captures(game_raw).unwrap();
         let game = Game {
-            button_a: Point {
-                x: captures.name("a_x").unwrap().as_str().parse().unwrap(),
-                y: captures.name("a_y").unwrap().as_str().parse().unwrap(),
-            },
-            button_b: Point {
-                x: captures.name("b_x").unwrap().as_str().parse().unwrap(),
-                y: captures.name("b_y").unwrap().as_str().parse().unwrap(),
-            },
-            target: Point {
-                x: captures.name("p_x").unwrap().as_str().parse().unwrap(),
-                y: captures.name("p_y").unwrap().as_str().parse().unwrap(),
-            },
+            p: captures.name("p_x").unwrap().as_str().parse::<f64>().unwrap() + 10000000000000.0,
+            q: captures.name("p_y").unwrap().as_str().parse::<f64>().unwrap() + 10000000000000.0,
+            a_x: captures.name("a_x").unwrap().as_str().parse::<f64>().unwrap(),
+            a_y: captures.name("a_y").unwrap().as_str().parse::<f64>().unwrap(),
+            b_x: captures.name("b_x").unwrap().as_str().parse::<f64>().unwrap(),
+            b_y: captures.name("b_y").unwrap().as_str().parse::<f64>().unwrap(),
         };
         match game.play() {
             Some(cost) => { total_cost += cost; }
